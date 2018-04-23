83ad78e7b30ae1f4da0b930d43c9c61ceb4d7484
M150117\student <greatjin1886@gmail.com>
2018-04-23 13:16:36 +0900

    employee VO 추가

--- vo/Employee.java
+++ vo/Employee.java
@@ -6,17 +6,37 @@ public class Employee {
 	private String empName; // 사원명
 	private String empTel;// 사원전화번호
 	private String empEmail;// 사원 이메일
-
+	private String empDept; // 사원 부서
+	private String empPw; //사원 비밀번호
+	
 	public Employee() {
 		super();
 	}
 
-	public Employee(int empNo, String empName, String empTel, String empEmail) {
+	public Employee(int empNo, String empName, String empTel, String empEmail, String empDept, String empPw) {
 		super();
 		this.empNo = empNo;
 		this.empName = empName;
 		this.empTel = empTel;
 		this.empEmail = empEmail;
+		this.empDept = empDept;
+		this.empPw = empPw;
+	}
+
+	public String getEmpPw() {
+		return empPw;
+	}
+
+	public void setEmpPw(String empPw) {
+		this.empPw = empPw;
+	}
+
+	public String getEmpDept() {
+		return empDept;
+	}
+
+	public void setEmpDept(String empDept) {
+		this.empDept = empDept;
 	}
 
 	public int getEmpNo() {
